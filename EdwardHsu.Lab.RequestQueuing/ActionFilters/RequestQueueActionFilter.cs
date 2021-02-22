e20bb045a1093356ff9170c5836aa5c8da6006bf
Edward Hsu <st89382000@gmail.com>
2021-02-22 23:31:58 +0800

    加入專案檔案。

---
+++ EdwardHsu.Lab.RequestQueuing/ActionFilters/RequestQueueActionFilter.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace EdwardHsu.Lab.RequestQueuing.ActionFilters
+{
+    public class RequestQueueActionFilter : IAsyncActionFilter
+    {
+        private static ConcurrentDictionary<string, ActionBlock<(ActionExecutionDelegate, TaskCompletionSource)>> _requestQueue
+            = new ConcurrentDictionary<string, ActionBlock<(ActionExecutionDelegate, TaskCompletionSource)>>();
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (context.ActionDescriptor is ControllerActionDescriptor cad)
+            {
+                var queueAttr = cad.MethodInfo.GetCustomAttribute<RequestQueueAttribute>();
+
+                var tcs = new TaskCompletionSource();
+
+                var queue = _requestQueue.GetOrAdd(queueAttr.Identifier, new ActionBlock<(ActionExecutionDelegate, TaskCompletionSource)>(async ((ActionExecutionDelegate _next, TaskCompletionSource _tcs) input) =>
+                {
+                    try
+                    {
+                        await input._next();
+                        input._tcs.SetResult();
+                    }
+                    catch (Exception e)
+                    {
+                        input._tcs.SetException(e);
+                    }
+                }));
+
+                queue.Post((next, tcs));
+
+                await tcs.Task;
+            }
+            else
+            {
+                await next();
+            }
+        }
+    }
+}
---
+++ EdwardHsu.Lab.RequestQueuing/ActionFilters/RequestQueueAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EdwardHsu.Lab.RequestQueuing.ActionFilters
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class RequestQueueAttribute : Attribute
+    {
+        public const string GLOBAL = "__GLOBAL__";
+        public string Identifier { get; set; } = GLOBAL;
+    }
+}
